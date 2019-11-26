c350ef4f8fd3d590b1aeae92ef1fa5c364cde267
forgi86 <marco.forgione1986@gmail.com>
2019-11-26 13:05:06 +0100

    More comments on ssmodels

--- torchid/ssmodels.py
+++ torchid/ssmodels.py
@@ -12,7 +12,8 @@ from torch.jit import Final
 
 class NeuralStateSpaceModel(nn.Module):
 
-    """ This class implements a state-space neural model
+    """ This class implements a state-space neural model with structure
+        x_{k+1} = NN_x(x_k, u_k)
 
      Attributes
      ----------
@@ -23,9 +24,6 @@ class NeuralStateSpaceModel(nn.Module):
      n_feat : int.
            number of units in the hidden layer
      """
-    n_x: Final[int]
-    n_u: Final[int]
-    n_feat: Final[int]
 
     def __init__(self, n_x, n_u, n_feat=64, init_small=True):
         super(NeuralStateSpaceModel, self).__init__()
@@ -44,34 +42,6 @@ class NeuralStateSpaceModel(nn.Module):
                     nn.init.normal_(m.weight, mean=0, std=1e-4)
                     nn.init.constant_(m.bias, val=0)
     
-    def forward(self, X,U):
-        XU = torch.cat((X,U),-1)
-        DX = self.net(XU)
-        return DX
-
-
-class NeuralStateSpaceModel2Hidden(nn.Module):
-    n_x: Final[int]
-    n_u: Final[int]
-    n_feat: Final[int]
-
-    def __init__(self, n_x, n_u, n_feat=32):
-        super(NeuralStateSpaceModel2Hidden, self).__init__()
-        self.n_x = n_x
-        self.n_u = n_u
-        self.n_feat = n_feat
-        self.net = nn.Sequential(
-            nn.Linear(n_x + n_u, n_feat),  # 2 states, 1 input
-            nn.Linear(n_feat, n_feat),
-            nn.ReLU(),
-            nn.Linear(n_feat, n_x)
-        )
-
-        #for m in self.net.modules():
-        #    if isinstance(m, nn.Linear):
-        #        nn.init.normal_(m.weight, mean=0, std=1e-4)
-        #        nn.init.constant_(m.bias, val=0)
-
     def forward(self, X, U):
         XU = torch.cat((X, U), -1)
         DX = self.net(XU)
@@ -79,26 +49,37 @@ class NeuralStateSpaceModel2Hidden(nn.Module):
 
 
 class NeuralStateSpaceModelLin(nn.Module):
+    """ This class implements a state-space neural model with structure
+        x_{k+1} = A_L x_k + B_K u_k + NN_x(x_k, u_k)
+
+     Attributes
+     ----------
+     n_x : int.
+           number of states
+     n_u : int.
+           number of inputs
+     n_feat : int.
+           number of units in the hidden layer
+     """
     def __init__(self, AL, BL):
         super(NeuralStateSpaceModelLin, self).__init__()
         self.net = nn.Sequential(
             nn.Linear(3, 64),  # 2 states, 1 input
             nn.ReLU(),
-            nn.Linear(64,2)
+            nn.Linear(64, 2)
         )
 
-        self.AL = nn.Linear(2,2, bias=False)
+        self.AL = nn.Linear(2, 2, bias=False)
         self.AL.weight = torch.nn.Parameter(torch.tensor(AL.astype(np.float32)), requires_grad=False)
-        self.BL = nn.Linear(1,2, bias=False)
+        self.BL = nn.Linear(1, 2, bias=False)
         self.BL.weight = torch.nn.Parameter(torch.tensor(BL.astype(np.float32)), requires_grad=False)
-
 
         for m in self.net.modules():
             if isinstance(m, nn.Linear):
                 nn.init.normal_(m.weight, mean=0, std=1e-2)
                 nn.init.constant_(m.bias, val=0)
     
-    def forward(self, X,U):
+    def forward(self, X, U):
         XU = torch.cat((X, U), -1)
         DX = self.net(XU)
         DX += self.AL(X) + self.BL(U)
@@ -109,12 +90,12 @@ class StateSpaceModelLin(nn.Module):
     def __init__(self, AL, BL):
         super(StateSpaceModelLin, self).__init__()
 
-        self.AL = nn.Linear(2,2, bias=False)
+        self.AL = nn.Linear(2, 2, bias=False)
         self.AL.weight = torch.nn.Parameter(torch.tensor(AL.astype(np.float32)), requires_grad=False)
-        self.BL = nn.Linear(1,2, bias=False)
+        self.BL = nn.Linear(1, 2, bias=False)
         self.BL.weight = torch.nn.Parameter(torch.tensor(BL.astype(np.float32)), requires_grad=False)
     
-    def forward(self, X,U):
+    def forward(self, X, U):
         DX = self.AL(X) + self.BL(U)
         return DX   
 
@@ -159,11 +140,11 @@ class CartPoleDeepStateSpaceModel(nn.Module):
         self.net = nn.Sequential(
             nn.Linear(5, 64),  # 4 states, 1 input
             nn.ReLU(),
-            nn.Linear(64,32), # 2 state equations (the other 2 are fixed by basic physics)
+            nn.Linear(64, 32), # 2 state equations (the other 2 are fixed by basic physics)
             nn.ReLU(),
             #nn.Linear(32, 32),  # 2 state equations (the other 2 are fixed by basic physics)
             #nn.ReLU(),
-            nn.Linear(32,2)
+            nn.Linear(32, 2)
         )
 
         if init_small:
@@ -172,12 +153,12 @@ class CartPoleDeepStateSpaceModel(nn.Module):
                     nn.init.normal_(m.weight, mean=0, std=1e-4)
                     nn.init.constant_(m.bias, val=0)
 
-        self.AL = nn.Linear(4,4, bias=False)
+        self.AL = nn.Linear(4, 4, bias=False)
         self.AL.weight = torch.nn.Parameter(torch.tensor([[0., Ts, 0., 0.],
                                                           [0., 0., 0., 0.],
                                                           [0., 0., 0., Ts],
                                                           [0., 0., 0., 0.]]), requires_grad=False)
-        self.WL = nn.Linear(2,4, bias=False)
+        self.WL = nn.Linear(2, 4, bias=False)
         self.WL.weight = torch.nn.Parameter(torch.tensor([[0., 0.],
                                                           [1., 0.],
                                                           [0., 0.],
